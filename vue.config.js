5488e278c6b979da8dbc4cd2d45f708e6580b891
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2022-01-31 14:10:55 +0300

    [/issues/8] dist for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/geme-of-life/'
+    : '/',
   configureWebpack: {
     resolve: {
       alias: {
